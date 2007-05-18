53db60c0d287e7b11f2056643d5183d595e39240
Graeme Mathieson <mathie@woss.name>
2007-05-18 17:36:38 +0000

    Refactor the pool initialisation so that it's less icky.
    git-svn-id: http://repos.rubaidh.com/zetta/trunk@6 68698aeb-e32f-0410-bc7e-ede830af38d2

--- ext/libzfs/libzfs.c
+++ ext/libzfs/libzfs.c
@@ -4,30 +4,32 @@
   #include <libzfs.h>
 #endif
 
-static VALUE my_zpool_initialize(VALUE self, VALUE pool_name, VALUE libzfs_handle)
+// We have to merge alloc and init here because we want to allocate the space
+// for the C data structure, but we also need the arguments passed to
+// initialize to do so.
+// 
+// FIXME: I'm still not sure this is the right way to do it!
+static VALUE my_zpool_new(int argc, VALUE *argv, VALUE klass)
 {
+  // FIXME: Make this robust!
+  VALUE pool_name = argv[0];
+  VALUE libzfs_handle = argv[1];
+  
   libzfs_handle_t *libhandle;
   zpool_handle_t  *zpool_handle;
-  
+
   Data_Get_Struct(libzfs_handle, libzfs_handle_t, libhandle);
 
   // FIXME: Should be +_canfail+ or not?
   zpool_handle = zpool_open_canfail(libhandle, StringValuePtr(pool_name));
-  
-  // FIXME: This is definitely got to be the wrong way to store the
-  // handle to the pool, but so far it appears to work.  Trouble is that I
-  // can't do it the way that Programming Ruby says, because that would
-  // involve calling +zpool_open()+ in the +alloc+ function, but we don't
-  // know the pool name there.
-  rb_iv_set(self, "@handle", Data_Wrap_Struct(rb_funcall3(self, rb_intern("class"), 0, 0), 0, zpool_close, zpool_handle));
 
-  return self;
+  return Data_Wrap_Struct(klass, 0, zpool_close, zpool_handle);
 }
 
 static VALUE my_zpool_get_name(VALUE self)
 {
   zpool_handle_t *zpool_handle;
-  Data_Get_Struct(rb_iv_get(self, "@handle"), zpool_handle_t, zpool_handle);
+  Data_Get_Struct(self, zpool_handle_t, zpool_handle);
   
   return rb_str_new2(zpool_get_name(zpool_handle));
 }
@@ -35,7 +37,7 @@ static VALUE my_zpool_get_name(VALUE self)
 static VALUE my_zpool_get_guid(VALUE self)
 {
   zpool_handle_t *zpool_handle;
-  Data_Get_Struct(rb_iv_get(self, "@handle"), zpool_handle_t, zpool_handle);
+  Data_Get_Struct(self, zpool_handle_t, zpool_handle);
   
   return ULL2NUM(zpool_get_guid(zpool_handle));
 }
@@ -43,7 +45,7 @@ static VALUE my_zpool_get_guid(VALUE self)
 static VALUE my_zpool_get_space_used(VALUE self)
 {
   zpool_handle_t *zpool_handle;
-  Data_Get_Struct(rb_iv_get(self, "@handle"), zpool_handle_t, zpool_handle);
+  Data_Get_Struct(self, zpool_handle_t, zpool_handle);
   
   return ULL2NUM(zpool_get_space_used(zpool_handle));
 }
@@ -51,7 +53,7 @@ static VALUE my_zpool_get_space_used(VALUE self)
 static VALUE my_zpool_get_space_total(VALUE self)
 {
   zpool_handle_t *zpool_handle;
-  Data_Get_Struct(rb_iv_get(self, "@handle"), zpool_handle_t, zpool_handle);
+  Data_Get_Struct(self, zpool_handle_t, zpool_handle);
   
   return ULL2NUM(zpool_get_space_total(zpool_handle));
 }
@@ -108,7 +110,7 @@ void Init_libzfs()
   rb_define_method(cLibZfs, "error_action", my_libzfs_error_action, 0);
   rb_define_method(cLibZfs, "error_description", my_libzfs_error_description, 0);
 
-  rb_define_method(cZpool, "initialize", my_zpool_initialize, 2);
+  rb_define_singleton_method(cZpool, "new", my_zpool_new, -1);
   rb_define_method(cZpool, "name", my_zpool_get_name, 0);
   rb_define_method(cZpool, "guid", my_zpool_get_guid, 0);
   rb_define_method(cZpool, "space_used", my_zpool_get_space_used, 0);
